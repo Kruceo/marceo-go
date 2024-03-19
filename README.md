8d9091686ffefbd346f308d171f92010b0dde591
kruceo <kruceo.com@gmail.com>
2024-03-19 11:05:27 -0300

    att: added args system

---
+++ README.md
@@ -0,0 +1,15 @@
+# Marceo Go
+
+## Building from sources
+
+```bash
+git clone https://github.com/Kruceo/marceo-go.git
+cd marceo-go
+go build
+```
+
+## Basic CLI Usage
+
+```bash 
+./marceo -i ./my/text.md -o ./my/output.html
+```
--- main.go
+++ main.go
@@ -1,22 +1,58 @@
-// main.go
-
 package main
 
 import (
-	"fmt"
+	"log"
 	"marceo/library/packs"
 	"os"
 )
 
-func main() {
+const version string = "0.4.2"
 
-	if len(os.Args) < 2 {
-		fmt.Println("File not found")
-		return
+func main() {
+	var inputText string = ""
+	var outputPath string = ""
+	for index, v := range os.Args {
+		if v == "-i" {
+			if len(os.Args) > index+1 {
+				argInput := os.Args[index+1]
+				file, err := os.ReadFile(argInput)
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				inputText = string(file)
+			} else {
+				log.Fatal("No input")
+			}
+		}
+
+		if v == "-o" {
+			if len(os.Args) > index+1 {
+				argInput := os.Args[index+1]
+				outputPath = argInput
+			} else {
+				log.Fatal("No input")
+			}
+
+		}
+		if v == "-v" {
+			println("v" + version)
+			break
+		}
+		if v == "--help" || v == "-h" {
+			println("-i $path$         ", "Read the input path file and parse to html.")
+			println("-o $path$         ", "Use the input path to write the results in a file.")
+			println("-v                ", "Show version.")
+			println("--help -h         ", "Show this message.")
+		}
 	}
-	str, err := os.ReadFile(os.Args[1])
-	if err != nil {
-		println("Error in file read")
+
+	var parsedText = packs.DefaultPack.Parse(inputText)
+
+	if outputPath != "" {
+		os.WriteFile(outputPath, []byte(parsedText), 0644)
+	} else if inputText != "" {
+		println(parsedText)
 	}
-	fmt.Println(packs.DefaultPack.Parse(string(str)))
 }
